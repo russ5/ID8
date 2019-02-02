cb096590f1a2e8bddef1115970a9a37b73cd8ec3
russ5 <32937206+russ5@users.noreply.github.com>
2019-02-02 13:37:02 -0500

    Renamed files!

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var chat = io.of('/chat');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/welcome', function(req, res){
-    res.sendFile(__dirname + '/welcome.html');
+app.get('/chat', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
 });
 
-io.on('connection', function(socket){
+chat.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        chat.emit('chat message', msg);
     });
 });
 
